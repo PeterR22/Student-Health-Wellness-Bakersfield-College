9dc8a7fc287173b743579aacfe589a2a1a294963
PeterR22 <39044599+PeterR22@users.noreply.github.com>
2020-04-08 14:19:28 -0700

    Create b-MAST.py
    Script created in collaboration with LMFT Rick Hobbs
    
    # Script Variables
    # Form -- The current form
    # ChangedObject -- The Object that has Changed
    # ChangeType -- The type of Change.  Valid Values: Add,Delete,Modification
    # LoadStatus -- Load Status.  Valid Values: Loading,Loaded# NOTE: This script can be recursive.  Do Not use global variables.  Setting a formobject's EditValue will cause this script to recurse.

---
+++ b-MAST.py
@@ -0,0 +1,11 @@
+#--------Name: Peter Rodriguez
+#--------Date: 04/04/2020
+#--------Information:
+
+from MediaHighway.Common.BLL.Forms import *
+
+def Run(form,changedObject,changeType,loadStatus):
+	pass
+
+
+if __loadStatus == LoadStatus.Loaded: Run(__form,__changedObject,__changeType,__loadStatus)
