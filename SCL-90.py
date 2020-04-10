fe1b56ab4ab47ddc054f4d42d2fca4a5b507cac6
PeterR22 <39044599+PeterR22@users.noreply.github.com>
2020-04-09 18:02:09 -0700

    Update SCL-90.py
    Edited tabs to be in compliance of Python syntax.

--- SCL-90.py
+++ SCL-90.py
@@ -13,25 +13,25 @@ def Run(form,changedObject,changeType,loadStatus):
 def Count_SCL90_Responses(form):
 	Number_of_Not_At_All_Response = 0 
 	Number_of_A_Little_Bit_Response = 0
-  Number_of_Moderately_Response = 0
-  Number_of_Quite_A_Bit_Response = 0
-  Number_of_Extremely_Response = 0
+  	Number_of_Moderately_Response = 0
+  	Number_of_Quite_A_Bit_Response = 0
+  	Number_of_Extremely_Response = 0
   
-  Not_At_All_Response_Value = 0 
-  A_Little_Bit_Response_Value = 1
-  Moderately_Response_Value = 2
-  Quite_A_Bit_Response_Value = 3
-  Extremely_Response_Value = 4
+  	Not_At_All_Response_Value = 0 
+  	A_Little_Bit_Response_Value = 1
+  	Moderately_Response_Value = 2
+  	Quite_A_Bit_Response_Value = 3
+  	Extremely_Response_Value = 4
   
-	for i in range(1,5):
+	for i in range(1,91):
 		question = form["q"+str(i)]
 		if question == None or question.EditValueAsString == "" : continue
 		idx = question.Selections.IndexOf(question.CurrentSelection)
 		if idx == 0: Number_of_Not_At_All_Response+= 1
 		if idx == 1: Number_of_A_Little_Bit_Response+= 1
-    if idx == 1: Number_of_Moderately_Response+= 1
-    if idx == 1: Number_of_Quite_A_Bit_Response+= 1
-    if idx == 1: Number_of_Extremely_Response+= 1
+    		if idx == 1: Number_of_Moderately_Response+= 1
+    		if idx == 1: Number_of_Quite_A_Bit_Response+= 1
+    		if idx == 1: Number_of_Extremely_Response+= 1
 
 	if form["Total_Yes_Response"]!= None: form["Total_Yes_Response"].EditValue = Number_of_Yes_Response
 	if form["Total_No_Response"]!= None: form["Total_No_Response"].EditValue = Number_of_No_Response
